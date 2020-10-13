d03be06bd4becef1fb9d84c0a2f53521feb03727
Mike North <michael.l.north@gmail.com>
2020-10-13 12:42:08 -0700

    api-extractor basic setup

--- src/index.ts
+++ src/index.ts
@@ -40,3 +40,5 @@ export function sum2(a: number, b: number): number {
   const sum = a + b;
   return sum;
 }
+
+export { subtract } from "./subtract";
---
+++ src/subtract.ts
@@ -0,0 +1,11 @@
+/**
+ * Calculate the difference between two numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ *
+ * @beta
+ */
+export function subtract(a: number, b: number): number {
+  return a - b;
+}
